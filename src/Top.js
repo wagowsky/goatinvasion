f496b0aa51c43f048b667def1b0f35e3426c9013
wagowsky <advakond@gmail.com>
2020-09-11 17:43:15 +0200

    started

--- src/Top.js
+++ src/Top.js
@@ -12,3 +12,49 @@ const Top = () => {
 }
 
 export default Top;
+
+
+
+// {
+//   "workbench.iconTheme": "material-icon-theme",
+//   "workbench.colorTheme": "BeardedTheme Anthracite",
+//   "explorer.confirmDelete": false,
+//   "window.zoomLevel": 2,
+//   "explorer.confirmDragAndDrop": false,
+//   "[javascript]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "editor.wordWrap": "on",
+//   "editor.minimap.enabled": false,
+//   "files.autoSave": "afterDelay",
+//   "javascript.updateImportsOnFileMove.enabled": "always",
+//   "[javascriptreact]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "emmet.extensionsPath": "",
+//   "editor.snippetSuggestions": "top",
+//   "[json]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "emmet.includeLanguages": { "javascript": "javascriptreact" },
+//   "files.associations": {
+//     "javascript": "javascriptreact",
+   
+//   },
+//   "editor.formatOnSave": true,
+//   "prettier.jsxSingleQuote": true,
+//   "prettier.singleQuote": true,
+//   "[jsonc]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "[html]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "[css]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "[vue]": {
+//     "editor.defaultFormatter": "octref.vetur"
+//   },
+//   "liveServer.settings.donotShowInfoMsg": true
+// }
