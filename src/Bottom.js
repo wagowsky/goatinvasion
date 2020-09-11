03a0d1aa9e3ec73fa2d0ff3bc0374b9f72836e17
wagowsky <advakond@gmail.com>
2020-09-11 23:13:57 +0200

    mobile done

--- src/Bottom.js
+++ src/Bottom.js
@@ -3,13 +3,14 @@ import bottom from "./assets/bottom.jpg"
 const Bottom = () => {
   return (
     <div className="bottom">
-      <h2>Contact</h2>
+      <h2 className="contact">Contact</h2>
       {/* <img className="bottom-img" src={bottom} /> */}
      <form >
-     <h2>Name</h2>
+     <h2 >Name</h2>
        <input type="text" name="name" />
        <h2>Email</h2>
        <input type="email" name="email" />
+       <h2>Message</h2>
        <textarea name="textarea" />
        <button type="submit">Send</button>
      </form>
--- src/Top.js
+++ src/Top.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import top from "./assets/top.jpg"
+import top from "./assets/mobile-top.jpg"
 const Top = () => {
   return (
     <div className="top">
-      <div className="black"></div>
+      <div className="black">
+        <div className="menuContainer">
+
+        <h2 className="menu menu1">Work</h2>
+        <br/>
+        <h2 className="menu menu2">Contact</h2>
+        </div>
+      </div>
     <img className="img" src={top} alt="top"/>
      {/* <div className="img"></div> */}
 <div className="black-invert"></div>
@@ -15,46 +22,4 @@ export default Top;
 
 
 
-// {
-//   "workbench.iconTheme": "material-icon-theme",
-//   "workbench.colorTheme": "BeardedTheme Anthracite",
-//   "explorer.confirmDelete": false,
-//   "window.zoomLevel": 2,
-//   "explorer.confirmDragAndDrop": false,
-//   "[javascript]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "editor.wordWrap": "on",
-//   "editor.minimap.enabled": false,
-//   "files.autoSave": "afterDelay",
-//   "javascript.updateImportsOnFileMove.enabled": "always",
-//   "[javascriptreact]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "emmet.extensionsPath": "",
-//   "editor.snippetSuggestions": "top",
-//   "[json]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "emmet.includeLanguages": { "javascript": "javascriptreact" },
-//   "files.associations": {
-//     "javascript": "javascriptreact",
-   
-//   },
-//   "editor.formatOnSave": true,
-//   "prettier.jsxSingleQuote": true,
-//   "prettier.singleQuote": true,
-//   "[jsonc]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "[html]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "[css]": {
-//     "editor.defaultFormatter": "esbenp.prettier-vscode"
-//   },
-//   "[vue]": {
-//     "editor.defaultFormatter": "octref.vetur"
-//   },
-//   "liveServer.settings.donotShowInfoMsg": true
-// }
+
