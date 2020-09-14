a7c633ff01809ab20974732cc2ae022f3c865070
wagowsky <advakond@gmail.com>
2020-09-15 00:38:59 +0200

    newest

--- src/Bottom.js
+++ src/Bottom.js
@@ -3,15 +3,16 @@ import bottom from "./assets/bottom.jpg"
 const Bottom = () => {
   return (
     <div className="bottom">
-      <h2>Contact</h2>
+      <h2 class="contact">Contact</h2>
       {/* <img className="bottom-img" src={bottom} /> */}
      <form >
      <h2>Name</h2>
        <input type="text" name="name" />
        <h2>Email</h2>
        <input type="email" name="email" />
+       <h2>Message</h2>
        <textarea name="textarea" />
-       <button type="submit">Send</button>
+       <button type="submit"><h3>Send</h3></button>
      </form>
     </div>
   );
--- src/Work.js
+++ src/Work.js
@@ -7,22 +7,30 @@ import logovalhalla from './assets/logovalhalla.png';
 const Work = () => {
   return (
     <div className='work'>
+      <h1>WORK</h1>
+      <div className='logos'>
+        <div className='project'>
+          <img className='logo' src={logovalhalla} alt='logovalhalla' />
 
-    <h1>WORK</h1>
-    <img className='logo' src={logovalhalla} alt='logovalhalla' />
-    
-      <h2>More</h2> 
-      <img className='logo' src={logonarnia} alt='logonarnia' />
-      <h2>More</h2>
-     
-      <img className='logo' src={logofotogalery} alt='logofotogalery' />
-      <h2>More</h2>
-      <img
-        className='logo'
-        src={logofantasyillustration}
-        alt='logofantasyillustration'
-      />
-      <h2>More</h2>
+          <h2>More</h2>
+        </div>
+        <div className='project'>
+          <img className='logo' src={logonarnia} alt='logonarnia' />
+          <h2>More</h2>
+        </div>
+        <div className='project'>
+          <img className='logo' src={logofotogalery} alt='logofotogalery' />
+          <h2>More</h2>
+        </div>
+        <div className='project'>
+          <img
+            className='logo'
+            src={logofantasyillustration}
+            alt='logofantasyillustration'
+          />
+          <h2>More</h2>
+        </div>
+      </div>
     </div>
   );
 };
